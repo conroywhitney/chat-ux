54b46825aff82e4e9e0229c423ffbc8e86fc8e8e
Conroy Whitney <conroy.whitney@gmail.com>
2023-06-28 21:12:10 -0500

    Able to do multi-part queries/function calls

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { nanoid } from "@/lib/utils";
-import { Chat } from "@/components/chat";
+import ChatExperimental from "@/components/chat-experimental";
 
 export const runtime = "edge";
 
 export default function IndexPage() {
   const id = nanoid();
 
-  return <Chat id={id} />;
+  return <ChatExperimental />;
 }
